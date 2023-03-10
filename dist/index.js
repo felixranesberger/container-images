b0111573f1ac9599796c3bf2f764c147afa40277
Felix Ranesberger <felix.ranesberger@in2code.de>
2023-03-10 13:56:17 +0100

    🔖 Release 0.1.8

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,47 @@
-const f = (s) => s.replace(
+const l = (s) => s.replace(
   /[A-Z]+(?![a-z])|[A-Z]/g,
-  (r, t) => (t ? "-" : "") + r.toLowerCase()
-), l = (s) => {
-  const { dataset: r } = s;
-  if (Object.keys(r).length === 0)
+  (e, r) => (r ? "-" : "") + e.toLowerCase()
+), g = (s) => {
+  const { dataset: e } = s;
+  if (Object.keys(e).length === 0)
     return;
-  const t = Object.entries(r).map(([e, n]) => [f(e), n]).filter(([e]) => (e == null ? void 0 : e.startsWith("source-")) || !1).map(([e, n]) => [e == null ? void 0 : e.replace("source-", ""), n]);
-  return Object.fromEntries(t);
-}, b = (s) => Object.entries(s).sort(([r], [t]) => r === "default" ? 1 : t === "default" || parseInt(t, 10) <= parseInt(r, 10) ? -1 : 1), d = (s) => {
-  const r = s.querySelector("source");
+  const r = Object.entries(e).map(([t, n]) => [l(t), n]).filter(([t]) => (t == null ? void 0 : t.startsWith("source-")) || !1).map(([t, n]) => [t == null ? void 0 : t.replace("source-", ""), n]);
+  return Object.fromEntries(r);
+}, b = (s) => Object.entries(s).sort(([e], [r]) => e === "default" ? 1 : r === "default" || parseInt(r, 10) <= parseInt(e, 10) ? -1 : 1), c = (s) => {
+  const e = s.querySelector("source");
+  if (e === null)
+    return;
+  const r = s.querySelector("img");
   if (r === null)
     return;
-  const t = s.querySelector("img");
-  if (t === null)
+  const t = g(e);
+  if (t === void 0)
     return;
-  const e = l(r);
-  if (e === void 0)
-    return;
-  const n = b(e);
-  new ResizeObserver(([a]) => {
-    const { width: c } = a.contentRect, o = n.find(([i]) => Number.isNaN(parseInt(i, 10)) ? !1 : parseInt(i, 10) <= c);
-    if (o === void 0) {
-      r.srcset = e.default;
+  const n = b(t);
+  new ResizeObserver(([u]) => {
+    const { width: d } = u.contentRect, i = n.find(([a]) => Number.isNaN(parseInt(a, 10)) ? !1 : parseInt(a, 10) <= d);
+    if (i === void 0) {
+      e.srcset = t.default;
       return;
     }
-    const [, u] = o;
-    r.srcset = u;
-  }).observe(t);
-}, v = (s) => {
-  s.forEach((r) => {
-    const t = {
+    const [, f] = i;
+    e.srcset = f;
+  }).observe(r);
+}, m = (s) => {
+  s.forEach((e) => {
+    const r = {
       rootMargin: "200px"
-    }, e = new IntersectionObserver(([n]) => {
-      n.isIntersecting && (d(r), e.disconnect());
-    }, t);
-    e.observe(r);
+    };
+    if (e.hasAttribute("data-container-images-loading") && e.getAttribute("data-container-images-loading") === "eager") {
+      c(e);
+      return;
+    }
+    const n = new IntersectionObserver(([o]) => {
+      o.isIntersecting && (c(e), n.disconnect());
+    }, r);
+    n.observe(e);
   });
 };
 export {
-  v as default
+  m as default
 };
--- dist/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (pictureTags: HTMLPictureElement[]) => void;
-export default _default;
